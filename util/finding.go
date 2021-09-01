0061c0a09e36b73696ec56f67f08d20c3f53491f
isp1r0 <ian.spiro@praetorian.com>
2021-09-01 16:30:00 -0700

    change verbose output to use parent function of input source (#35)
    * change verbose output to use parent function of input source
    
    * strip args from source parent
    
    * strip arguments from source function signature

--- util/finding.go
+++ util/finding.go
@@ -70,11 +70,10 @@ func OutputFinding(finding Finding) {
 		cyan := color.New(color.FgCyan).SprintFunc()
 		green := color.New(color.FgGreen).SprintFunc()
 
-		parentFunctionArgs := finding.Vulnerable_Function.ParentFunction
-		parentFunctionNoArgs := StripArguments(finding.Vulnerable_Function.ParentFunction)
+		sinkParentNoArgs := StripArguments(finding.Vulnerable_Function.ParentFunction)
 
 		fmt.Printf("\n(%s) %s\n\n", cyan(finding.Type), yellow(finding.message))
-		fmt.Printf("%s:%d\nVulnerable Function: [ %s ]\n", finding.Vulnerable_Function.SourceFilename, finding.Vulnerable_Function.SourceLineNum, parentFunctionNoArgs)
+		fmt.Printf("%s:%d\nVulnerable Function: [ %s ]\n", finding.Vulnerable_Function.SourceFilename, finding.Vulnerable_Function.SourceLineNum, sinkParentNoArgs)
 		fmt.Printf("      %d:\t%s\n", finding.Vulnerable_Function.SourceLineNum-1, GrabSourceCode(finding.Vulnerable_Function.SourceFilename, finding.Vulnerable_Function.SourceLineNum-1))
 		fmt.Printf("    > %d:\t%s\n", finding.Vulnerable_Function.SourceLineNum, finding.Vulnerable_Function.SourceCode)
 		fmt.Printf("      %d:\t%s\n", finding.Vulnerable_Function.SourceLineNum+1, GrabSourceCode(finding.Vulnerable_Function.SourceFilename, finding.Vulnerable_Function.SourceLineNum+1))
@@ -83,7 +82,7 @@ func OutputFinding(finding Finding) {
 
 			source := finding.Untrusted_Source[0]
 			fmt.Printf("\n%s:%d\n", source.SourceFilename, source.SourceLineNum)
-			fmt.Printf("Source of Untrusted Input: [ %s ]\n", parentFunctionNoArgs)
+			fmt.Printf("Source of Untrusted Input: [ %s ]\n", StripArguments(source.ParentFunction))
 			fmt.Printf("      %d:\t%s\n", source.SourceLineNum-1, GrabSourceCode(source.SourceFilename, source.SourceLineNum-1))
 			fmt.Printf("    > %d:\t%s\n", source.SourceLineNum, source.SourceCode)
 			fmt.Printf("      %d:\t%s\n", source.SourceLineNum+1, GrabSourceCode(source.SourceFilename, source.SourceLineNum+1))
@@ -93,7 +92,7 @@ func OutputFinding(finding Finding) {
 				fmt.Printf("\nUntrusted Input Source:")
 				for _, source := range finding.Untrusted_Source {
 					fmt.Printf("%s:%d:\n[ %s ]\n>>>\t%s\n", source.SourceFilename,
-						source.SourceLineNum, parentFunctionArgs, strings.TrimLeft(source.SourceCode, " \t"))
+						source.SourceLineNum, StripArguments(source.ParentFunction), strings.TrimLeft(source.SourceCode, " \t"))
 				}
 			}
 
